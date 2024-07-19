cdc5c7f974d63a789decabec5cc5b858b5ed682b
Jason Vann <vannjt1@icloud.com>
2024-07-18 22:43:38 -0400

    merged data complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,49 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+  const [ planets, setPlanets ] = useState([])
+  const [ people, setPeople ] = useState([])
+
+  useEffect(() => {
+    axios.get(urlPlanets)
+    .then(res => {
+      setPlanets(res.data);
+      console.log(res.data);
+    }).catch(err => console.log(err));
+  }, [])
+
+  useEffect(() => {
+    axios.get(urlPeople)
+    .then(res => {
+      setPeople(res.data);
+      console.log(res.data);
+    }).catch(err => console.log(err));
+  }, [])
+
+  let merged = [];
+  people.forEach(person => {
+    let res = {
+      ...person,
+      homeworld: people.map(id => {
+        let homeworld = planets.find(planetObj => planetObj.id === id);
+        return `${homeworld}`;
+      })
+    }
+    merged.push(res);
+    console.log(merged);
+  })
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {merged.map(merge => 
+      <Character 
+        key={merge.id} 
+        name={merge.name} 
+        home={merge.homeworld} />)}
+      
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+function Character(props) { // ❗ Add the props
+  const {name, home} = props;
   // ❗ Create a state to hold whether the homeworld is rendering or not
+
+    const [isToggled, setToggle] = useState(false);
+
+    const handleClick = () => {
+      setToggle(!isToggled);
+    }
+  
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+
   return (
-    <div>
+    <div className='character-card' onClick={handleClick}>
       {/* Use the same markup with the same attributes as in the mock */}
+      <h3 className='character-name'>{name} </h3> 
+      {isToggled ? 'ON' : 'OFF'}
     </div>
   )
 }
